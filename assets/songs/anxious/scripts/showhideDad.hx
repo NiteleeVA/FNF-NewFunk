68603ca3fade13aff9d27bc7d137bf34206c050e
NiteleeVA <rihothegamer@gmail.com>
2025-04-01 22:57:59 +0300

    chapter 1 testing yippee
    send help to my sanity xd

---
+++ assets/songs/anxious/scripts/showhideDad.hx
@@ -0,0 +1,9 @@
+// Function to hide the opponent (Dad)
+function hideOpponent()
+    dad.alpha = 0  // Makes Dad fully transparent (hidden)
+end
+
+// Function to show the opponent (Dad)
+function showOpponent()
+    dad.alpha = 1  // Makes Dad fully visible
+end
